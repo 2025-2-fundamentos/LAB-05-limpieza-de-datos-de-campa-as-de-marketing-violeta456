5f35d21ff0d6f53c1837bd9396ce5eae85497fd3
violeta <159485730+violeta456@users.noreply.github.com>
2025-11-16 19:04:05 -0500

    Lab 05. Completo.
    Uso de archivos comprimidos sin descomprimirlos,  limpieza de algunas columnas y creación de csv

--- homework/homework.py
+++ homework/homework.py
@@ -3,6 +3,106 @@ Escriba el codigo que ejecute la accion solicitada.
 """
 
 # pylint: disable=import-outside-toplevel
+from zipfile import ZipFile
+import glob
+import pandas as pd
+import os 
+
+def client(dataFrame):
+
+    #Select dataframe columns
+    df_client = dataFrame[["client_id", "age", "job", "marital", "education", "credit_default", "mortgage"]].copy()
+
+    #Clean job column
+    df_client["job"] = df_client["job"].str.replace(".","")
+    df_client["job"] = df_client["job"].str.replace("-","_")
+
+    #Clean education column
+    df_client["education"] =df_client["education"].str.replace(".","_")
+    df_client["education"] =df_client["education"].replace("unknown", pd.NA)
+
+    #Clean credit_default column
+    df_client["credit_default"] = df_client["credit_default"].apply(lambda x: 1 if x=="yes"else 0)
+
+    #Clean mortage column
+    df_client["mortgage"] = df_client["mortgage"].apply(lambda x: 1 if x=="yes" else 0)
+    return df_client
+
+def campaign(dataFrame):
+
+    def month_to_number(month):
+        month_dict = {
+            "jan": "01",
+            "feb": "02",
+            "mar": "03",
+            "apr": "04",
+            "may": "05",
+            "jun": "06",
+            "jul": "07",
+            "aug": "08",
+            "sep": "09",
+            "oct": "10",
+            "nov": "11",
+            "dec": "12",
+        }
+        return month_dict.get(month.lower())
+
+    #Select columns
+    df_compaigns = dataFrame[["client_id", "number_contacts", "contact_duration", "previous_campaign_contacts", "previous_outcome", "campaign_outcome"]].copy()
+
+    #Clean previous_outcome column
+    df_compaigns["previous_outcome"] =dataFrame["previous_outcome"].apply(lambda x: 1 if x =="success" else 0)
+    #Clean campaign_outcome column
+    df_compaigns["campaign_outcome"] =dataFrame["campaign_outcome"].apply(lambda x: 1 if x =="yes" else 0)
+
+    #Create last_contact_day column
+    df_compaigns["last_contact_date"]=(
+        "2022-"
+        +dataFrame["month"].apply(month_to_number)
+        +"-"+dataFrame["day"].astype(str))
+    
+    return df_compaigns
+
+
+def economics(dataFrame):
+    #Select columns
+    df_economics = dataFrame[["client_id", "cons_price_idx", "euribor_three_months"]].copy()
+    return df_economics
+
+
+def concatenate_dataframes(dataframes):
+    return pd.concat(dataframes, ignore_index=True)
+
+
+def create_output_directory(output_dir):
+    if os.path.exists(output_dir):
+        for file in glob.glob(f"{output_dir}/*"):
+            os.remove(file)
+        os.rmdir(output_dir)
+    
+    os.mkdir(output_dir)
+    return output_dir
+
+
+def get_info_files(rute):
+  
+    info = []
+    # Iterate over all ZIP files matching the given path pattern
+    for zip_file in glob.glob(rute):
+        # Open the ZIP file for reading
+        with ZipFile(zip_file, "r") as zip_file_1:
+            # Get the name of the first file inside the ZIP (assumed to be the CSV)
+            zinfo = zip_file_1.infolist()[0].filename
+            # Open the CSV file inside the ZIP without extracting
+            with zip_file_1.open(zinfo) as file:
+                df = pd.read_csv(file)
+                info.append(df)
+    
+    return info
+                
+
+
+
 
 
 def clean_campaign_data():
@@ -50,8 +150,17 @@ def clean_campaign_data():
 
     """
 
-    return
+    data_frames = get_info_files("files/input/*.zip")
+    concatenated_df = concatenate_dataframes(data_frames)
+
+    create_output_directory("files/output/")
+    df_client = client(concatenated_df)
+    df_client.to_csv(r"files/output/client.csv", index=False)
+    df_campaign = campaign(concatenated_df)
+    df_campaign.to_csv(r"files/output/campaign.csv", index=False)
+    df_economics = economics(concatenated_df)
+    df_economics.to_csv(r"files/output/economics.csv", index=False)
+    
 
 
-if __name__ == "__main__":
-    clean_campaign_data()
+clean_campaign_data()
